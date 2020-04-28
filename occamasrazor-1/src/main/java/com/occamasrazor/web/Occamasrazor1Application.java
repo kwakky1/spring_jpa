f4b57083f1d99b2a9e8e48e6389406535cfff7a9
bit2 <kwakky1@gmail.com>
2020-04-28 12:45:42 +0900

    2020.04.28 // 12:45

---
+++ occamasrazor-1/src/main/java/com/occamasrazor/web/Occamasrazor1Application.java
@@ -0,0 +1,13 @@
+package com.occamasrazor.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Occamasrazor1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Occamasrazor1Application.class, args);
+	}
+
+}
